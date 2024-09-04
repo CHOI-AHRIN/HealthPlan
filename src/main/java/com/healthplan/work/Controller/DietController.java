93c340fb117c8ea909979ba9c006f9c2298ca7fb
CHOI-AHRIN <ahrin9610@naver.com>
2024-09-04 12:54:36 +0900

    ahrin: 커밋합니다

--- src/main/java/com/healthplan/work/Controller/DietController.java
+++ src/main/java/com/healthplan/work/Controller/DietController.java
@@ -1,16 +1,51 @@
 package com.healthplan.work.Controller;
 
 
+import com.healthplan.work.dao.DietMapper;
+import com.healthplan.work.vo.NewsEntity;
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Controller
+@RequestMapping("/community")
 public class DietController {
 
     // 로거 삽입
-    private Logger logger = LoggerFactory.getLogger(DietController.class);
+    private static final Logger logger = LoggerFactory.getLogger(DietController.class);
 
+    @Autowired
+    DietMapper diet;
+
+
+    // test
+  //  @RequestMapping(value = "/get", method = RequestMethod.GET)
+    @GetMapping
+    @ResponseBody
+    public String getReminder(){
+        return "my reminder";
+    }
+
+
+    @RequestMapping (value="/list", method = RequestMethod.GET)
+    public @ResponseBody Map<String, Object> news() throws Exception {
+
+        Map<String, Object> rtnObj = new HashMap<>();
+
+        List<NewsEntity> newsList = diet.list();
+        logger.info("news->" + newsList.toString());
+
+        rtnObj.put("news_list", newsList);
+        return rtnObj;
+    }
 
 }
--- src/main/java/com/healthplan/work/dao/DietMapper.java
+++ src/main/java/com/healthplan/work/dao/DietMapper.java
@@ -1,4 +1,13 @@
 package com.healthplan.work.dao;
 
+import com.healthplan.work.vo.NewsEntity;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
 public interface DietMapper {
+
+        public List<NewsEntity> list();
+
 }
