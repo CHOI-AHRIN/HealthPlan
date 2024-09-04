dc006369312c277d1f771bfb75573ce10421a698
CHOI-AHRIN <ahrin9610@naver.com>
2024-09-04 15:34:07 +0900

    ahrin: 예시 커밋

---
+++ src/main/java/com/healthplan/work/config/SecurityConfig.java
@@ -0,0 +1,18 @@
+package com.healthplan.work.config;
+
+import com.healthplan.work.Controller.DietController;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@Log4j2
+
+public class SecurityConfig {
+
+//    @Bean
+//    public DietController dietController(){
+//        return DietController(pattern: "/diet/**/*");
+//    }
+
+}
