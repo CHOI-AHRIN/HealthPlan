e3ce71732be889016f551cb771b7104342acc936
chajiwon81 <aqwfrgtt@naver.com>
2024-09-05 14:45:41 +0900

    Exercise.service

---
+++ src/main/java/com/healthplan/work/service/ExerciseService.java
@@ -0,0 +1,30 @@
+package com.healthplan.work.service;
+
+import java.util.List;
+
+import com.healthplan.work.vo.ExerciseEntity;
+
+public interface ExerciseService {
+
+  public void regist(ExerciseEntity community) throws Exception;
+
+  public ExerciseEntity read(Integer cno) throws Exception;
+
+  public void modify(ExerciseEntity community) throws Exception;
+
+  public void remove(Integer cno) throws Exception;
+
+  public List<ExerciseEntity> listAll() throws Exception;
+/*
+  public List<ExerciseEntity> listCriteria(SearchCriteria cri) throws Exception;
+
+  public int listCountCriteria(SearchCriteria cri) throws Exception;
+
+  public List<ExerciseEntity> listSearchCriteria(SearchCriteria cri)  throws Exception;
+
+  public int listSearchCount(SearchCriteria cri) throws Exception;*/
+  
+  public List<String> getAttach(Integer bno)throws Exception;
+  
+
+}
