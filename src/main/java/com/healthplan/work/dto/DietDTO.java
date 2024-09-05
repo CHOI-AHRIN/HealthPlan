d7acad6913a06c829aeaa31f9e3506e9dd41a3af
CHOI-AHRIN <ahrin9610@naver.com>
2024-09-05 11:57:30 +0900

    ahrin: 거지같아도 .. 커밋

---
+++ src/main/java/com/healthplan/work/dto/DietDTO.java
@@ -0,0 +1,27 @@
+package com.healthplan.work.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+
+
+
+
+public class DietDTO {
+
+    private Long cno;
+    private String title;
+    private String contents;
+    private String uuid;
+    private Long mno;
+    private LocalDateTime bdate;
+
+}
