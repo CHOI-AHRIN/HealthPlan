5d3f740738b6c3832ed4575d01f41e9ecef40ed4
CHOI-AHRIN <ahrin9610@naver.com>
2024-09-29 21:39:25 +0900

    ahrin: 챌린지 , 멤버 수정사항

--- src/main/java/com/healthplan/work/dao/ChallengeMapper.java
+++ src/main/java/com/healthplan/work/dao/ChallengeMapper.java
@@ -3,16 +3,23 @@ package com.healthplan.work.dao;
 import com.healthplan.work.vo.ChallengeEntity;
 import com.healthplan.work.vo.SearchCriteria;
 import org.apache.ibatis.annotations.Mapper;
+
 import java.util.List;
 
 @Mapper
 public interface ChallengeMapper {
-        List<ChallengeEntity> selectChallengeList(SearchCriteria cri) throws Exception;
-        public int selectChallengeCount(SearchCriteria cri) throws Exception;
+    List<ChallengeEntity> selectChallengeList(SearchCriteria cri) throws Exception;
 
-        public ChallengeEntity selectChallengeRead(int bno) throws Exception;
+    public int selectChallengeCount(SearchCriteria cri) throws Exception;
 
-        void insertChallenge(ChallengeEntity vo) throws Exception;
-        void updateChallenge(ChallengeEntity vo) throws Exception;
-        void deleteChallenge(int bno) throws Exception;
+    public ChallengeEntity selectChallengeRead(int bno) throws Exception;
+
+    void insertChallenge(ChallengeEntity vo) throws Exception;
+
+    void updateChallenge(ChallengeEntity vo) throws Exception;
+
+    void deleteChallenge(int bno) throws Exception;
+
+    public void updateChallengeCount(int bno) throws Exception;
+
 }
