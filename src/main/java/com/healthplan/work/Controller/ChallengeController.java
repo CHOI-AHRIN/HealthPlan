7b764576347980c8354df05a84f73a35573dde4f
CHOI-AHRIN <ahrin9610@naver.com>
2024-10-28 09:55:17 +0900

    ahrin: challenge controller, url에 api 추가

--- src/main/java/com/healthplan/work/Controller/ChallengeController.java
+++ src/main/java/com/healthplan/work/Controller/ChallengeController.java
@@ -16,7 +16,7 @@ import java.util.Map;
 
 @RestController
 @Log4j2
-@RequestMapping("/challenge")
+@RequestMapping("/api/challenge")
 public class ChallengeController {
 
     private static final Logger logger = LoggerFactory.getLogger(ChallengeController.class);
