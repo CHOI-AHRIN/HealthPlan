8d81b08aabee43529e4ad06527d7056ae029a8f3
hocil <elmobong150000@gmail.com>
2024-11-05 12:19:15 +0900

    챌린지 파일첨부 기능추가 (진행 중)

--- src/main/java/com/healthplan/work/service/ChallengeService.java
+++ src/main/java/com/healthplan/work/service/ChallengeService.java
@@ -1,14 +1,19 @@
 package com.healthplan.work.service;
 
-import com.healthplan.work.dao.ChallengeMapper;
-import com.healthplan.work.vo.ChallengeEntity;
-import com.healthplan.work.vo.SearchCriteria;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import com.healthplan.work.dao.ChallengeMapper;
+import com.healthplan.work.vo.ChallengeEntity;
+import com.healthplan.work.vo.ImageDTO;
+import com.healthplan.work.vo.SearchCriteria;
+
+import lombok.extern.log4j.Log4j2;
 
 @Service
+@Log4j2
 public class ChallengeService {
 
     @Autowired
@@ -18,25 +23,49 @@ public class ChallengeService {
         return challengeMapper.selectChallengeList(cri);
     }
 
+    //챌린지 Count
     public int selectChallengeCount(SearchCriteria cri) throws Exception {
         return challengeMapper.selectChallengeCount(cri);
     }
 
+    //챌린지 읽기
     public ChallengeEntity selectChallengeRead(int bno) throws Exception {
         challengeMapper.updateChallengeCount(bno);
+        // logger.info("29 Line: SubscribeSerive Lession Count ====> " + bno);
         return challengeMapper.selectChallengeRead(bno);
     }
 
+    // 챌린지 작성
     public void challengeInsert(ChallengeEntity vo) throws Exception {
         challengeMapper.insertChallenge(vo);
-    }
 
+        List<ImageDTO> imageDTOList = vo.getImageDTOList();
+
+        if (imageDTOList != null && !imageDTOList.isEmpty()) {
+            for (ImageDTO imageDTO : imageDTOList) {
+                String imgName = imageDTO.getThumbnailURL();
+                String imgURL = imageDTO.getImageURL();
+                String uuid = imageDTO.getUuid();
+                String path = imageDTO.getPath();
+                String imgType = imageDTO.getImgType();
+
+                challengeMapper.addAttach(imgName, imgURL, uuid, path, imgType);
+            }
+        }
+    }
+    
+    // 챌린지 수정
     public void challengeUpdate(ChallengeEntity vo) throws Exception {
         challengeMapper.updateChallenge(vo);
     }
 
+    // 챌린지 삭제
     public void challengeDelete(int bno) throws Exception {
         challengeMapper.deleteChallenge(bno);
     }
+
+
+
+
 
 }
