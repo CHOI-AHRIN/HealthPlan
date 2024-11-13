633ea7ecb20b77f7a54f8b1bc21b05618246d5d6
hocil <elmobong150000@gmail.com>
2024-11-13 16:50:29 +0900

    ddl문 수정(정리)

--- src/main/resources/mybatis/sql/ddl.sql
+++ src/main/resources/mybatis/sql/ddl.sql
@@ -4,20 +4,18 @@
 -- 회원 테이블
 CREATE TABLE Member (
     mNo NUMBER NOT NULL, -- 회원번호
-    uuid VARCHAR2(230), -- 아이디
+    uuid VARCHAR2(30), -- 아이디
     upw VARCHAR2(1000), -- 비밀번호
     phone VARCHAR2(100), -- 전화번호
-    name VARCHAR2(220), -- 이름
+    name VARCHAR2(20), -- 이름
     mType CHAR(1), -- 회원타입
     email VARCHAR2(100), -- 이메일
     regdate DATE, -- 가입일
-    sessionKey VARCHAR2(250), -- 세션키
-    sessionLimit TIMESTAMP, -- 세션만료시간
-    ssType CHAR(1), -- 구독타입
+    ssType CHAR(1), -- 구독타입(S/A/B/C/N)
     PRIMARY KEY (mNo)
 );
 
--- 보드(챌린지, FAQ) 테이블
+-- 보드(챌린지) 테이블
 CREATE TABLE Board (
     bNo NUMBER NOT NULL, -- 보드번호
     mNo NUMBER, -- 회원번호
@@ -26,18 +24,10 @@ CREATE TABLE Board (
     bContents VARCHAR2(2000), -- 글내용
     wDate DATE, -- 작성일자
     bCounts NUMBER, -- 조회수
-    REPLYCNT NUMBER, -- 댓글수
+    Replycnt NUMBER, -- 댓글수
     PRIMARY KEY (bNo),
     FOREIGN KEY (mNo) REFERENCES Member (mNo) ON DELETE CASCADE
 );
-
-ALTER TABLE NAttach
-ADD bno NUMBER;
-
-ALTER TABLE NAttach
-ADD CONSTRAINT fk_nattach_board
-FOREIGN KEY (bno) REFERENCES BOARD(bno)
-ON DELETE CASCADE;
 
 -- 보드 댓글 테이블
 CREATE TABLE BReply (
@@ -54,9 +44,8 @@ CREATE TABLE BReply (
 -- 구독 테이블
 CREATE TABLE Subscribe (
     sNo NUMBER NOT NULL, -- 구독번호
-    uuid VARCHAR2(230), -- 아이디
+    uuid VARCHAR2(30), -- 아이디
     mNo NUMBER NOT NULL, -- 회원번호
-    lNo NUMBER, -- 구독상품번호
     sType CHAR(1), -- 구독상품타입
     Title VARCHAR2(100), -- 글제목
     Contents VARCHAR2(2000), -- 글내용
@@ -77,35 +66,9 @@ CREATE TABLE SReply (
     sNo NUMBER, -- 구독번호
     rComment VARCHAR2(1000), -- 댓글내용
     rRegdate DATE, -- 댓글작성일
-    UREGDATE DATE, -- 댓글수정일
+    uRegdate DATE, -- 댓글수정일
     PRIMARY KEY (rNo),
     FOREIGN KEY (sNo) REFERENCES Subscribe (sNo) ON DELETE CASCADE,
-    FOREIGN KEY (mNo) REFERENCES Member (mNo) ON DELETE CASCADE
-);
-
--- 커뮤니티 테이블
-CREATE TABLE Community (
-    cNo NUMBER NOT NULL, -- 커뮤니티번호
-    mNo NUMBER, -- 회원번호
-    cType CHAR(1), -- 커뮤니티소분류
-    Title VARCHAR2(100), -- 글제목
-    Contents VARCHAR2(2000), -- 글내용
-    wDate DATE, -- 작성일자
-    Counts NUMBER, -- 조회수
-    replycnt NUMBER, -- 댓글수
-    PRIMARY KEY (cNo),
-    FOREIGN KEY (mNo) REFERENCES Member (mNo) ON DELETE CASCADE
-);
-
--- 커뮤니티 댓글 테이블
-CREATE TABLE CReply (
-    rNo NUMBER NOT NULL, -- 댓글번호
-    mNo NUMBER, -- 회원번호
-    cNo NUMBER, -- 커뮤니티번호
-    rComment VARCHAR2(1000), -- 댓글내용
-    rRegdate DATE, -- 댓글작성일
-    PRIMARY KEY (rNo),
-    FOREIGN KEY (cNo) REFERENCES Community (cNo) ON DELETE CASCADE,
     FOREIGN KEY (mNo) REFERENCES Member (mNo) ON DELETE CASCADE
 );
 
@@ -113,24 +76,10 @@ CREATE TABLE CReply (
 CREATE TABLE Point (
     pNo NUMBER NOT NULL, -- 포인트번호
     mNo NUMBER, -- 회원번호
-    pSource VARCHAR2(100), -- 포인트출처
-    pcount NUMBER,
+    pSource VARCHAR2(30), -- 포인트출처
+    pcount NUMBER, -- 포인트량
     pDate DATE, -- 포인트생성일
     PRIMARY KEY (pNo),
-    FOREIGN KEY (mNo) REFERENCES Member (mNo) ON DELETE CASCADE
-);
-
--- 주문 테이블
-CREATE TABLE Orders (
-    oNo NUMBER NOT NULL, -- 주문번호
-    mNo NUMBER, -- 회원번호
-    oType CHAR(1), -- 주문타입
-    status CHAR(1), -- 상태
-    amount NUMBER, -- 금액
-    price NUMBER, -- 가격
-    oComment VARCHAR2(250), -- 주문코멘트
-    oDate DATE, -- 주문일자
-    PRIMARY KEY (oNo),
     FOREIGN KEY (mNo) REFERENCES Member (mNo) ON DELETE CASCADE
 );
 
@@ -139,15 +88,17 @@ CREATE TABLE NATTACH (
     UUID VARCHAR2(200),
     IMGNAME VARCHAR2(200),
     SNO NUMBER,
+    BNO NUMBER,
     ANO NUMBER,
     PATH VARCHAR2(200),
     IMAGEURL VARCHAR2(200),
     IMGTYPE CHAR(1 BYTE),
     REGDATE DATE,
-    FOREIGN KEY (SNO) REFERENCES Subscribe (sNo) ON DELETE CASCADE
+    FOREIGN KEY (SNO) REFERENCES Subscribe (SNO) ON DELETE CASCADE
+    FOREIGN KEY (BNO) REFERENCES BOARD (BNO) ON DELETE CASCADE
 );
 
--- 시퀀스
+-- <<시퀀스>>
 -- 보드 댓글 번호
 CREATE SEQUENCE BREPLY_SEQ MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1
 START WITH 1 NOCACHE;
@@ -176,7 +127,7 @@ START WITH 1 NOCACHE;
 CREATE SEQUENCE SNO_SEQ MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1
 START WITH 1 NOCACHE;
 
--- 기본 쿼리
+-- <<기본 쿼리>>
 -- PasswordEncryptor.java 실행해서 터미널에 프린트 된 비밀번호를 UPW의 VALUE 값에다 삽입 후 전체 실행
 INSERT INTO
     MEMBER (
@@ -210,29 +161,281 @@ INSERT INTO
     )
 VALUES (
         PNO_SEQ.NEXTVAL,
-        MNO_SEQ.CURRVAL,
+        0,
         '0',
         '회원가입',
         SYSDATE
     );
 
 -- 챌린지 게시판
-INSERT INTO
-    BOARD (
-        bno,
-        mno,
-        title,
-        bcontents,
-        wdate,
-        bcounts
-    )
-VALUES (
-        BOARD_SEQ.NEXTVAL,
-        1,
-        '제목입니다',
-        '내용입니다',
-        SYSDATE,
-        0
-    );
+INSERT INTO BOARD (
+    BNO,
+    MNO,
+    title,
+    bcontents,
+    wdate,
+    bcounts,
+    replycnt
+) VALUES (
+    BOARD_SEQ.NEXTVAL,
+    0,
+    '하루 만 보 걷기',
+    '목표: 하루에 10,000보 이상 걷기\n효과: 심혈관 건강 증진, 칼로리 소모',
+    SYSDATE,
+    0,
+    0
+);
+
+INSERT INTO BOARD (
+    BNO,
+    MNO,
+    title,
+    bcontents,
+    wdate,
+    bcounts,
+    replycnt
+) VALUES (
+    BOARD_SEQ.NEXTVAL,
+    0,
+    '스쿼트 100회 도전',
+    '목표: 하루 동안 스쿼트 100회 수행\n효과: 하체 근력 강화, 코어 안정성 향상',
+    SYSDATE,
+    0,
+    0
+);
+
+INSERT INTO BOARD (
+    BNO,
+    MNO,
+    title,
+    bcontents,
+    wdate,
+    bcounts,
+    replycnt
+) VALUES (
+    BOARD_SEQ.NEXTVAL,
+    0,
+    '팔굽혀펴기 50회 챌린지',
+    '목표: 하루에 팔굽혀펴기 50회 완료\n효과: 상체 근력 강화, 체력 향상',
+    SYSDATE,
+    0,
+    0
+);
+
+INSERT INTO BOARD (
+    BNO,
+    MNO,
+    title,
+    bcontents,
+    wdate,
+    bcounts,
+    replycnt
+) VALUES (
+    BOARD_SEQ.NEXTVAL,
+    0,
+    '플랭크 5분 유지하기',
+    '목표: 총 5분 동안 플랭크 자세 유지 (나누어서 진행 가능)\n효과: 코어 근육 강화, 자세 개선',
+    SYSDATE,
+    0,
+    0
+);
+
+INSERT INTO BOARD (
+    BNO,
+    MNO,
+    title,
+    bcontents,
+    wdate,
+    bcounts,
+    replycnt
+) VALUES (
+    BOARD_SEQ.NEXTVAL,
+    0,
+    '러닝 또는 조깅 3km 완주',
+    '목표: 하루에 3km 러닝 또는 조깅하기\n효과: 심폐 지구력 향상, 스트레스 해소',
+    SYSDATE,
+    0,
+    0
+);
+
+INSERT INTO BOARD (
+    BNO,
+    MNO,
+    title,
+    bcontents,
+    wdate,
+    bcounts,
+    replycnt
+) VALUES (
+    BOARD_SEQ.NEXTVAL,
+    0,
+    '버피 테스트 50회',
+    '목표: 하루에 버피 50회 수행\n효과: 전신 근력 강화, 심박수 증가',
+    SYSDATE,
+    0,
+    0
+);
+
+INSERT INTO BOARD (
+    BNO,
+    MNO,
+    title,
+    bcontents,
+    wdate,
+    bcounts,
+    replycnt
+) VALUES (
+    BOARD_SEQ.NEXTVAL,
+    0,
+    '줄넘기 1,000회',
+    '목표: 하루에 줄넘기 1,000회 완료\n효과: 심폐 지구력 향상, 하체 근력 강화',
+    SYSDATE,
+    0,
+    0
+);
+
+INSERT INTO BOARD (
+    BNO,
+    MNO,
+    title,
+    bcontents,
+    wdate,
+    bcounts,
+    replycnt
+) VALUES (
+    BOARD_SEQ.NEXTVAL,
+    0,
+    '자전거 타기 10km 도전',
+    '목표: 하루에 10km 자전거 타기\n효과: 하체 근력 강화, 심폐 지구력 향상, 칼로리 소모',
+    SYSDATE,
+    0,
+    0
+);
+
+INSERT INTO BOARD (
+    BNO,
+    MNO,
+    title,
+    bcontents,
+    wdate,
+    bcounts,
+    replycnt
+) VALUES (
+    BOARD_SEQ.NEXTVAL,
+    0,
+    '1분 동안 플랭크 유지',
+    '목표: 하루에 총 1분 동안 플랭크 자세 유지\n효과: 코어 근력 강화, 자세 개선, 전신 안정성 향상',
+    SYSDATE,
+    0,
+    0
+);
+
+INSERT INTO BOARD (
+    BNO,
+    MNO,
+    title,
+    bcontents,
+    wdate,
+    bcounts,
+    replycnt
+) VALUES (
+    BOARD_SEQ.NEXTVAL,
+    0,
+    '계단 오르기 30층 도전',
+    '목표: 하루 동안 계단 30층 오르기\n효과: 하체 근력 및 심폐 지구력 향상, 체력 증진',
+    SYSDATE,
+    0,
+    0
+);
+
+INSERT INTO BOARD (
+    BNO,
+    MNO,
+    title,
+    bcontents,
+    wdate,
+    bcounts,
+    replycnt
+) VALUES (
+    BOARD_SEQ.NEXTVAL,
+    0,
+    '크런치 200회 챌린지',
+    '목표: 하루 동안 크런치 200회 수행\n효과: 복근 강화, 코어 안정성 증대, 자세 개선',
+    SYSDATE,
+    0,
+    0
+);
+
+INSERT INTO BOARD (
+    BNO,
+    MNO,
+    title,
+    bcontents,
+    wdate,
+    bcounts,
+    replycnt
+) VALUES (
+    BOARD_SEQ.NEXTVAL,
+    0,
+    '요가 30분 수련',
+    '목표: 하루에 30분 동안 요가 수련하기\n효과: 유연성 향상, 정신적 안정, 근육 이완 및 스트레스 해소',
+    SYSDATE,
+    0,
+    0
+);
+
+INSERT INTO BOARD (
+    BNO,
+    MNO,
+    title,
+    bcontents,
+    wdate,
+    bcounts,
+    replycnt
+) VALUES (
+    BOARD_SEQ.NEXTVAL,
+    0,
+    '1시간 스트레칭 챌린지',
+    '목표: 하루에 1시간 동안 전신 스트레칭\n효과: 유연성 향상, 근육 긴장 완화, 부상 예방',
+    SYSDATE,
+    0,
+    0
+);
+
+INSERT INTO BOARD (
+    BNO,
+    MNO,
+    title,
+    bcontents,
+    wdate,
+    bcounts,
+    replycnt
+) VALUES (
+    BOARD_SEQ.NEXTVAL,
+    0,
+    '등산 5km 도전',
+    '목표: 하루에 5km 등산하기\n효과: 하체 근력 강화, 심폐 지구력 향상, 자연 속에서 스트레스 해소',
+    SYSDATE,
+    0,
+    0
+);
+
+INSERT INTO BOARD (
+    BNO,
+    MNO,
+    title,
+    bcontents,
+    wdate,
+    bcounts,
+    replycnt
+) VALUES (
+    BOARD_SEQ.NEXTVAL,
+    0,
+    '덤벨 5kg로 100회 리프팅',
+    '목표: 하루에 5kg 덤벨을 이용해 100회 리프팅\n효과: 상체 근력 강화, 체력 증진, 근육 톤 업',
+    SYSDATE,
+    0,
+    0
+);
 
 commit;
